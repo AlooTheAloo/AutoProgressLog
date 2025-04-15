85896d379444602bc547a50df4147c22dfce6925
AlooTheAloo4250 <philip29313@hotmail.com>
2025-04-15 17:54:25 -0400

    update version

--- apl-website/src/pages/downloads.vue
+++ apl-website/src/pages/downloads.vue
@@ -3,66 +3,102 @@
     class="absolute flex h-screen w-screen flex-col items-center justify-center overflow-hidden rounded-lg lg:w-full md:w-full pointer-events-none"
   >
     <Ripple
+      :space-between-circle="200"
+      :wave-speed="100"
       class="bg-white/5 [mask-image:linear-gradient(to_bottom,white,transparent)]"
       circle-class="border-[hsl(var(--primary))] bg-[#0000]/25 dark:bg-[#fff]/25 rounded-full"
     />
   </div>
 
-  <div
-    class="flex justify-center items-center flex-col h-screen w-screen gap-10"
-  >
-    <div
-      class="w-screen max-w-[70rem] lg:px-10 flex lg:justify-between justify-center"
+  <div class="h-screen w-screen flex justify-center items-center">
+    <Motion
+      as="div"
+      :initial="{ opacity: 0, y: 40, filter: 'blur(10px)' }"
+      :while-in-view="{
+        opacity: 1,
+        y: 0,
+        filter: 'blur(0px)',
+      }"
+      :transition="{
+        delay: 0,
+        duration: 1.5,
+        ease: 'easeInOut',
+      }"
+      class="flex justify-center items-center flex-col gap-10"
     >
-      <div class="flex flex-col justify-center items-center lg:items-start">
-        <h1
-          class="lg:text-start text-center text-3xl font-semibold dark:text-neutral-200 w-full"
-        >
-          You're almost there!
-        </h1>
-        <h2
-          class="lg:text-start text-center text-neutral-600 text-2xl font-light dark:text-neutral-200 w-full"
-        >
-          Get ready to start your journey with APL.
-        </h2>
-        <RainbowButton class="mt-5 w-52 p-0" v-if="platform !== 'other'">
-          Download for {{ nicerName }}</RainbowButton
-        >
-        <div
-          v-else
-          class="mt-5 border-neutral-300 border-2 bg-neutral-200 w-fit px-2 h-10 rounded-lg cursor-not-allowed flex items-center justify-center text-neutral-500 select-none"
-        >
-          APL is not available for your platform yet
-        </div>
-      </div>
-
-      <div class="lg:block hidden">
-        <div class="flex">
-          <img :src="macos_apl" width="300" />
-        </div>
-      </div>
-    </div>
-
-    <div
-      class="flex gap-2 sm:gap-5 flex-wrap h-fit w-screen max-w-[70rem] px-10 justify-center"
-    >
-      <figure
-        v-for="item in items"
-        class="transition-all duration-200 relative w-72 h-fit overflow-hidden rounded-xl border border-gray-950/[.1] bg-white p-7 hover:bg-gray-100 dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+      <div
+        class="w-screen max-w-[70rem] lg:px-10 flex lg:justify-between justify-center"
       >
-        <div class="flex flex-row items-center gap-2">
-          <component :is="item.icon" class="w-[32px] h-[32px] text-black" />
-          <div class="flex flex-col">
-            <span class="text-xl font-medium dark:text-white">
-              {{ item.name }}
-            </span>
+        <div class="flex flex-col justify-center items-center lg:items-start">
+          <h1
+            class="lg:text-start text-center text-3xl font-semibold dark:text-neutral-200 w-full"
+          >
+            You're almost there!
+          </h1>
+          <h2
+            class="lg:text-start text-center text-neutral-600 text-2xl font-light dark:text-neutral-200 w-full"
+          >
+            Get ready to start your journey with APL.
+          </h2>
+          <RainbowButton
+            class="mt-5 w-52 p-0"
+            v-if="platform !== 'other'"
+            @click="externalOpen"
+          >
+            Download for {{ nicerName }}</RainbowButton
+          >
+          <div
+            v-else
+            class="mt-5 border-neutral-300 border-2 bg-neutral-200 w-fit px-2 h-10 rounded-lg cursor-not-allowed flex items-center justify-center text-neutral-500 select-none"
+          >
+            APL is not available for your platform yet
+          </div>
+
+          <Button class="p-0 mt-2" variant="link" @click="openGithub">
+            Other download options
+          </Button>
+        </div>
+
+        <div class="lg:block hidden">
+          <div class="flex">
+            <img :src="macos_apl" width="300" />
           </div>
         </div>
-        <blockquote class="mt-2 text-sm">
-          {{ item.body }}
-        </blockquote>
-      </figure>
-    </div>
+      </div>
+
+      <div
+        class="flex gap-2 sm:gap-5 flex-wrap h-fit w-screen max-w-[70rem] px-10 justify-center"
+      >
+        <Motion
+          v-for="(item, index) in items"
+          as="div"
+          :initial="{ opacity: 0, y: 40, filter: 'blur(10px)' }"
+          :while-in-view="{
+            opacity: 1,
+            y: 0,
+            filter: 'blur(0px)',
+          }"
+          :transition="{
+            delay: index * 0.2,
+            duration: 1,
+            ease: 'easeInOut',
+          }"
+          class="transition-colors relative w-72 h-fit overflow-hidden rounded-xl border border-gray-950/[.1] bg-white p-7 hover:bg-gray-100 dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+        >
+          <div class="flex flex-row items-center gap-2">
+            <component :is="item.icon" class="w-[32px] h-[32px] text-black" />
+            <div class="flex flex-col">
+              <span class="text-xl font-medium dark:text-white">
+                {{ item.name }}
+              </span>
+            </div>
+          </div>
+          <blockquote class="mt-2 text-sm">
+            {{ item.body }}
+          </blockquote>
+        </Motion>
+      </div>
+    </Motion>
   </div>
 </template>
 
@@ -72,6 +108,10 @@ import { BoxIcon, DownloadIcon, RocketIcon } from "lucide-vue-next";
 import macos_apl from "../assets/APL_Macbook.png";
 import { computed, onMounted, ref } from "vue";
 import RainbowButton from "@/components/ui/rainbow-button/RainbowButton.vue";
+import confetti from "canvas-confetti";
+import { Button } from "@/components/ui/button";
+import { Motion } from "motion-v";
+
 const items = [
   {
     name: "Download the app",
@@ -81,16 +121,16 @@ const items = [
   {
     name: "Install the program",
     icon: BoxIcon,
-    body: "Select your platform and download the executable.",
+    body: "Open the downloaded file and follow the instructions.",
   },
   {
     name: "Launch APL",
     icon: RocketIcon,
-    body: "Select your platform and download the executable.",
+    body: "Launch the program and start tracking your progress!",
   },
 ];
 
-type Platform = "windows" | "mac" | "linux" | "other";
+type Platform = "windows" | "mac" | "linux" | "other" | "all";
 
 const platform = ref<Platform>(getPlatform());
 
@@ -114,14 +154,7 @@ const nicerName = computed(() => {
 });
 
 const url = computed(() => {
-  switch (platform.value) {
-    case "windows":
-      return "https://github.com/AlooTheAloo/AutoProgressLog/releases/download/v1.0.0/AutoProgressLog.exe";
-    case "mac":
-      return "https://github.com/AlooTheAloo/AutoProgressLog/releases/download/v1.0.0/AutoProgressLog.dmg";
-    case "linux":
-      return "https://github.com/AlooTheAloo/AutoProgressLog/releases/download/v1.0.0/AutoProgressLog.AppImage";
-  }
+  return urls.value.find((x) => x.platform === platform.value)?.url;
 });
 
 const urls = ref<
@@ -131,11 +164,71 @@ const urls = ref<
   }[]
 >([]);
 
+function externalOpen() {
+  const end = Date.now() + 0.1 * 1000; // 3 seconds
+  const colors = ["#a786ff", "#fd8bbc", "#eca184", "#f8deb1"];
+
+  // Frame function to trigger confetti cannons
+  function frame() {
+    if (Date.now() > end) return;
+
+    // Left side confetti cannon
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      startVelocity: 40,
+      origin: { x: 0, y: 0.5 },
+      colors: colors,
+    });
+
+    // Right side confetti cannon
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      startVelocity: 40,
+      origin: { x: 1, y: 0.5 },
+      colors: colors,
+    });
+
+    requestAnimationFrame(frame); // Keep calling the frame function
+  }
+  frame();
+  window.open(url.value, "_blank");
+}
+
 onMounted(() => {
   const BACKEND_URL = "https://apl.chromaserver.net/downloadLinks";
   fetch(BACKEND_URL).then(async (x) => {
-    const data = await x.json();
-    console.log(data);
+    const data: {
+      windowsUrl: string;
+      macUrl: string;
+      linuxUrl: string;
+      releasesUrl: string;
+    } = await x.json();
+    urls.value = [
+      {
+        platform: "windows",
+        url: data.windowsUrl,
+      },
+      {
+        platform: "mac",
+        url: data.macUrl,
+      },
+      {
+        platform: "linux",
+        url: data.linuxUrl,
+      },
+      {
+        platform: "all",
+        url: data.releasesUrl,
+      },
+    ];
   });
 });
+
+function openGithub() {
+  window.open(urls.value.find((x) => x.platform === "all")?.url, "_blank");
+}
 </script>
