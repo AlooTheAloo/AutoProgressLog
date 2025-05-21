003d35668a8779759fc89384f657c12153080c86
AlooTheAloo4250 <aloothealoo4250@gmail.com>
2025-05-21 19:54:30 -0400

    ts-ignore it just works

--- apl-frontend/src/main.ts
+++ apl-frontend/src/main.ts
@@ -1,11 +1,11 @@
-import {createApp} from "vue";
-import {dialog} from "./util/DialogRenderer/dialog";
+import { createApp } from "vue";
+import { dialog } from "./util/DialogRenderer/dialog";
 import App from "./App.vue";
-import {createMemoryHistory, createRouter, RouteRecordRaw} from "vue-router";
+import { createMemoryHistory, createRouter, RouteRecordRaw } from "vue-router";
 import "./style.css";
 import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
-import {definePreset} from "@primevue/themes";
+import { definePreset } from "@primevue/themes";
 import Ripple from "primevue/ripple";
 import routes from "./pages/routes/routes";
 import "primeicons/primeicons.css";
@@ -20,56 +20,56 @@ import VueWriter from "vue-writer";
 import VueApexCharts from "vue3-apexcharts";
 
 const router = createRouter({
-    history: createMemoryHistory(),
-    routes: routes,
+  history: createMemoryHistory(),
+  routes: routes,
 });
 
 const Noir = definePreset(Aura, {
-    semantic: {
-        primary: {
-            50: "{zinc.50}",
-            100: "{zinc.100}",
-            200: "{zinc.200}",
-            300: "{zinc.300}",
-            400: "{zinc.400}",
-            500: "{zinc.500}",
-            600: "{zinc.600}",
-            700: "{zinc.700}",
-            800: "{zinc.800}",
-            900: "{zinc.900}",
-            950: "{zinc.950}",
-        },
-        colorScheme: {
-            light: {
-                primary: {
-                    color: "{zinc.950}",
-                    inverseColor: "#ffffff",
-                    hoverColor: "{zinc.900}",
-                    activeColor: "{zinc.800}",
-                },
-                highlight: {
-                    background: "{zinc.950}",
-                    focusBackground: "{zinc.700}",
-                    color: "#ffffff",
-                    focusColor: "#ffffff",
-                },
-            },
-            dark: {
-                primary: {
-                    color: "{zinc.50}",
-                    inverseColor: "{zinc.950}",
-                    hoverColor: "{zinc.100}",
-                    activeColor: "{zinc.200}",
-                },
-                highlight: {
-                    background: "rgba(250, 250, 250, .16)",
-                    focusBackground: "rgba(250, 250, 250, .24)",
-                    color: "rgba(255,255,255,.87)",
-                    focusColor: "rgba(255,255,255,.87)",
-                },
-            },
-        },
+  semantic: {
+    primary: {
+      50: "{zinc.50}",
+      100: "{zinc.100}",
+      200: "{zinc.200}",
+      300: "{zinc.300}",
+      400: "{zinc.400}",
+      500: "{zinc.500}",
+      600: "{zinc.600}",
+      700: "{zinc.700}",
+      800: "{zinc.800}",
+      900: "{zinc.900}",
+      950: "{zinc.950}",
     },
+    colorScheme: {
+      light: {
+        primary: {
+          color: "{zinc.950}",
+          inverseColor: "#ffffff",
+          hoverColor: "{zinc.900}",
+          activeColor: "{zinc.800}",
+        },
+        highlight: {
+          background: "{zinc.950}",
+          focusBackground: "{zinc.700}",
+          color: "#ffffff",
+          focusColor: "#ffffff",
+        },
+      },
+      dark: {
+        primary: {
+          color: "{zinc.50}",
+          inverseColor: "{zinc.950}",
+          hoverColor: "{zinc.100}",
+          activeColor: "{zinc.200}",
+        },
+        highlight: {
+          background: "rgba(250, 250, 250, .16)",
+          focusBackground: "rgba(250, 250, 250, .24)",
+          color: "rgba(255,255,255,.87)",
+          focusColor: "rgba(255,255,255,.87)",
+        },
+      },
+    },
+  },
 });
 
 dayjs.extend(duration);
@@ -78,25 +78,27 @@ dayjs.extend(advancedFormat);
 
 const app = createApp(App);
 
+// @ts-ignore
 app.config.globalProperties.$dialog = dialog;
 
-app.use(ConfirmationService)
-    .use(ToastService)
-    .use(router)
-    .use(VueWriter as any)
-    .use(PrimeVue, {
-        theme: {
-            preset: Noir,
-            options: {
-                darkModeSelector: ".dark",
-            },
-        },
-        ripple: true,
-    })
-    .use(VueApexCharts as any)
-    .directive("ripple", Ripple)
-    .directive("tooltip", Tooltip)
-    .mount("#app")
-    .$nextTick(() => {
-        postMessage({payload: "removeLoading"}, "*");
-    });
+app
+  .use(ConfirmationService)
+  .use(ToastService)
+  .use(router)
+  .use(VueWriter as any)
+  .use(PrimeVue, {
+    theme: {
+      preset: Noir,
+      options: {
+        darkModeSelector: ".dark",
+      },
+    },
+    ripple: true,
+  })
+  .use(VueApexCharts as any)
+  .directive("ripple", Ripple)
+  .directive("tooltip", Tooltip)
+  .mount("#app")
+  .$nextTick(() => {
+    postMessage({ payload: "removeLoading" }, "*");
+  });
