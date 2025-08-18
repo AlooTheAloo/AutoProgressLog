69c3fd47e74284070a2267cb21bde426a4705a67
Youssef Hamdouni <youssef.hamdouni.yh@gmail.com>
2025-08-18 16:28:45 -0400

    Forgot to actually add them lmao

--- apl-server/src/routes/user/index.ts
+++ apl-server/src/routes/user/index.ts
@@ -2,9 +2,11 @@ import {Elysia} from "elysia";
 import {configRoutes} from "./config";
 import {verifyProviderRoutes} from "./verifyProvider";
 import {syncRoute} from "./sync";
+import {meRoutes} from "./me";
 
 export const userRoutes = new Elysia({name: 'user-routes'}).group('/user', app =>
     app.use(configRoutes)
         .use(syncRoute)
+        .use(meRoutes)
         .use(verifyProviderRoutes)
 );
